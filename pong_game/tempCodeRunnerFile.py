614fdfcc0dac7a6c0df514a213e5f895b579fc41
lueksche <deibel_lukas@gmx.de>
2024-01-11 11:35:58 +0100

    initial commit: pong game - move paddles - ball bouncing - score keeping - increase ball speed

--- pong_game/tempCodeRunnerFile.py
+++ pong_game/tempCodeRunnerFile.py
@@ -1,65 +1,36 @@
-from turtle import Turtle, Screen
+from turtle import Turtle
 
-STARTING_POSITION_LEFT = ((-480,0),(-480,20),(-480,-20))
-STARTING_POSITION_RIGHT = ((480,0),(480,20),(480,-20))
 MOVING_DISTANCE = 20
-UP = 90
-DOWN = 270
 
 class Paddle(Turtle):
     def __init__(self):
-        self.starting_position = []
-        self.paddle_sgements_list = []
         self.controls_directory = {}
+        self.shapesize(stretch_wid=5, stretch_len=1)
     
     def up_key(self):
-        for segment in self.paddle_sgements_list:
-            self.setheading(UP)
-            segment.forward(MOVING_DISTANCE)
+        if self.ycor() < 330:
+            new_ycor = self.ycor() + MOVING_DISTANCE
+            self.goto(self.xcor(),new_ycor)
     
     def down_key(self):
-        for segment in self.paddle_sgements_list:
-            self.setheading(DOWN)
-            segment.forward(20)
-    
+        if self.ycor() > - 330:
+            new_ycor = self.ycor() - MOVING_DISTANCE
+            self.goto(self.xcor(),new_ycor)
+
     def initiate_paddle(self, side):
+        paddle = Turtle(shape="square")
+        paddle.penup()
+        paddle.color("white")
+    
         if side == "left":
-            for position in STARTING_POSITION_LEFT: 
-                self.add_segments(position)
+            self.goto(-350,0)
             self.controls_directory["W"] = self.up_key
-            self.controls_directory["A"] = self.down_key
+            self.controls_directory["S"] = self.down_key
 
         else:
-            for position in STARTING_POSITION_RIGHT:
-                self.add_segments(position)
+            self.goto(350,0)
             self.controls_directory["Up"] = self.up_key
             self.controls_directory["Down"] = self.down_key
-            
 
-    def add_segments(self, position):        
-        paddle_segment = Turtle(shape="square")
-        paddle_segment.penup()
-        paddle_segment.color("white")
-        paddle_segment.goto(position)
-        self.paddle_sgements_list.append(paddle_segment)
-
-
-#Initiating screen
-
-screen = Screen()
-
-#Setting up screen
-
-screen = Screen()
-screen.setup(width=1000, height=600)
-screen.bgcolor("black")
-screen.title("My Pong Game")
-screen.tracer(0)
-
-test = Paddle()
-test.color("white")
-test.forward(20)
-
-
-screen.exitonclick()
-    
+paddle = Paddle()
+paddle.initiate_paddle("left")
